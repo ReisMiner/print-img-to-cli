68d7f3f991e777df2b4d13aefeb4dd48c2dce2e9
ReisMiner <r1miner@outlook.com>
2022-06-29 21:10:42 +0200

    added new command args

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,22 +1,42 @@
+import org.apache.commons.cli.*;
+
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException, IOException {
         BufferedImage img = null;
         File f;
         String lookup = " .,-:;/(*#$%&";
 
-        if (args.length < 1) {
-            System.out.println("please specify an image path");
-            return;
+        Options options = new Options();
+
+        Option input = new Option("i", "input", true, "input file path");
+        input.setRequired(true);
+        options.addOption(input);
+
+        Option output = new Option("o", "output", true, "output file");
+        output.setRequired(false);
+        options.addOption(output);
+
+        CommandLineParser parser = new DefaultParser();
+        HelpFormatter formatter = new HelpFormatter();
+        CommandLine cmd = null;
+
+        try {
+            cmd = parser.parse(options, args);
+        } catch (ParseException e) {
+            System.out.println(e.getMessage());
+            formatter.printHelp("Image to Text", options);
+            System.exit(1);
         }
 
         try {
-            f = new File(args[0]);
+            f = new File(cmd.getOptionValue("i"));
             img = ImageIO.read(f);
         } catch (IOException e) {
             e.printStackTrace();
@@ -61,7 +81,14 @@ public class Main {
                 out.append(lookup.toCharArray()[a / 20]).append(" ");
         }
 
-        System.out.println(out);
 
+        if (cmd.hasOption("o")) {
+            File tmp = new File(cmd.getOptionValue("o"));
+            FileWriter fw = new FileWriter(tmp);
+            fw.write(out.toString());
+            fw.close();
+        } else {
+            System.out.println(out);
+        }
     }
 }
